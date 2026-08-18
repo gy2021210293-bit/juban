rikkahub/rikkahub

fix(mcp): 工具调用前刷新 OAuth 令牌并按需重连
callTool 此前不检查令牌新鲜度，长期空闲连接可能带过期 access token 调用。
现在调用前调用 ensureFreshToken，令牌刷新后用新配置重连以更新 transport header。

app/src/main/java/me/rerere/rikkahub/data/ai/mcp/McpManager.kt
@@ -152,9 +152,21 @@ class McpManager(
 
     suspend fun callTool(serverId: Uuid, toolName: String, args: JsonObject): List<UIMessagePart> {
         val entry = clients.entries.find { it.key.id == serverId }
-        val client = entry?.value
+        var client = entry?.value
             ?: return listOf(UIMessagePart.Text("Failed to execute tool, because no such mcp client for the tool"))
-        val config = entry.key
+        var config = entry.key
+
+        // 调用前确保 OAuth 令牌新鲜。若发生刷新，已连接的 transport 仍携带过期令牌
+        val freshConfig = ensureFreshToken(config)
+        if (freshConfig.commonOptions.oauth?.accessToken != config.commonOptions.oauth?.accessToken) {
+            Log.i(TAG, "callTool: token refreshed, reconnecting ${config.commonOptions.name}")
+            addClient(freshConfig)
+            val newEntry = clients.entries.find { it.key.id == serverId }
+                ?: return listOf(UIMessagePart.Text("Failed to execute tool, because no such mcp client for the tool"))
+            client = newEntry.value
+            config = newEntry.key
+        }
+
         Log.i(TAG, "callTool: $toolName / $args (server: ${config.commonOptions.name})")
 
         if (client.transport == null) client.connect(getTransport(config))

gy2021210293-bit/juban

(cherry picked from commit c49be6fefd93a2245b5630eb7599604eb6731004)

app/src/main/java/me/rerere/rikkahub/data/ai/mcp/McpManager.kt
@@ -162,9 +162,20 @@ class McpManager(
 
     suspend fun callTool(serverId: Uuid, toolName: String, args: JsonObject): List<UIMessagePart> {
         val pair = clients[serverId]
-        val client = pair?.second
+        var client = pair?.second
             ?: return listOf(UIMessagePart.Text("Failed to execute tool, because no such mcp client for the tool"))
-        val config = pair.first
+        var config = pair.first
+
+        // 调用前确保 OAuth 令牌新鲜。若发生刷新，已连接的 transport 仍携带过期令牌
+        val freshConfig = ensureFreshToken(config)
+        if (freshConfig.commonOptions.oauth?.accessToken != config.commonOptions.oauth?.accessToken) {
+            Log.i(TAG, "callTool: token refreshed, reconnecting ${config.commonOptions.name}")
+            addClient(freshConfig)
+            val newEntry = clients[serverId]
+                ?: return listOf(UIMessagePart.Text("Failed to execute tool, because no such mcp client for the tool"))
+            client = newEntry.second
+            config = newEntry.first
+        }
         Log.i(TAG, "callTool: $toolName / $args (server: ${config.commonOptions.name})")
 
         if (client.transport == null) client.connect(getTransport(config))